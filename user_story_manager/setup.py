4e6cbb1ca4b1759f978f47e279f0f82fbd08d049
keligeri <kel.gergo@gmail.com>
2017-02-16 13:00:25 +0100

    Repo ordering

---
+++ user_story_manager/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='user_story_manager',
+    packages=['user_story_manager'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
